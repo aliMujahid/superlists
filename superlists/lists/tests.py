c9a8898538a2f4195d52e96830a75821c2e05659
Mujahid <mhmmdalimujahid@gmail.com>
2022-05-18 16:36:15 +0600

    Basic view now returns minimal html

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,3 +1,4 @@
+from django.http import HttpRequest
 from django.urls import resolve
 from django.test import TestCase
 
@@ -7,3 +8,11 @@ class IndexTest(TestCase):
     def test_root_url_resolves_to_index_view(self):
         found = resolve('/')
         self.assertEqual(found.func, index)
+
+    def test_index_returns_correct_html(self):
+        request = HttpRequest()
+        response = index(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
