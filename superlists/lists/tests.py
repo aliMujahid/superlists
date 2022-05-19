0c66de2e34ff5f20f4e395ad96907edc8faa55e8
Mujahid <mhmmdalimujahid@gmail.com>
2022-05-19 18:41:18 +0600

    New url, view and test for creating list and refactor index

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -10,22 +10,6 @@ class IndexTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'lists/index.html')
 
-    def test_can_save_POST_requests(self):
-        response = self.client.post('/', data={'item_text':'new item text'})
-        
-        self.assertEqual(Item.objects.count(), 1)
-        item = Item.objects.first()
-        self.assertEqual(item.text, 'new item text')
-
-    def test_redirect_after_post(self):
-        response = self.client.post('/', data={'item_text':'new item text'})
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
-
-    def test_only_save_item_when_necessary(self):
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(), 0)
-
 
 class ViewListTest(TestCase):
     def test_uses_list_template(self):
@@ -40,6 +24,19 @@ class ViewListTest(TestCase):
 
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
+
+
+class NewListTest(TestCase):
+    def test_can_save_POST_requests(self):
+        response = self.client.post('/lists/new', data={'item_text':'new item text'})
+        
+        self.assertEqual(Item.objects.count(), 1)
+        item = Item.objects.first()
+        self.assertEqual(item.text, 'new item text')
+
+    def test_redirect_after_post(self):
+        response = self.client.post('/lists/new', data={'item_text':'new item text'})
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
 
 
 class ItemModelTest(TestCase):
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -3,13 +3,12 @@ from django.http import HttpResponse
 
 from lists.models import Item
 
-def index(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
-        
+def index(request):    
     return render(request, 'lists/index.html')
 
 def view_list(request):
     return render(request, 'lists/list.html', {'items':Item.objects.all()})
-        
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')         
