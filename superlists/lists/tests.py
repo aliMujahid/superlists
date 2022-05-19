58450ae27dbfa2dc6ec7b548da465977d0f4054f
Mujahid <mhmmdalimujahid@gmail.com>
2022-05-19 10:47:41 +0600

    Refactor index to use template

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -5,14 +5,6 @@ from django.test import TestCase
 from lists.views import index
 
 class IndexTest(TestCase):
-    def test_root_url_resolves_to_index_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, index)
-
     def test_index_returns_correct_html(self):
-        request = HttpRequest()
-        response = index(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'lists/index.html')
