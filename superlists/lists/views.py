d9f2f1ead7294d3f2d1562b410e11768085cc3c3
Mujahid <mhmmdalimujahid@gmail.com>
2022-05-19 19:52:03 +0600

    New URL for adding item to existing list. FT passes

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -6,10 +6,18 @@ from lists.models import Item, List
 def index(request):    
     return render(request, 'lists/index.html')
 
-def view_list(request):
-    return render(request, 'lists/list.html', {'items':Item.objects.all()})
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    items = Item.objects.filter(list=list_)
+    return render(request, 'lists/list.html', {'list':list_})
 
 def new_list(request):
     list_ = List.objects.create()
     Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/the-only-list-in-the-world/')         
+    return redirect(f'/lists/{list_.id}/')         
+
+def add_item(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect(f'/lists/{list_id}/')         
+    
--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -21,6 +21,7 @@ from lists import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('lists/new', views.new_list, name='new_list'),
-    path('lists/the-only-list-in-the-world/', views.view_list, name='view_list'),
+    path('lists/<int:list_id>/', views.view_list, name='view_list'),
+    path('lists/<int:list_id>/add_item', views.add_item, name='add_item'),
     path('admin/', admin.site.urls),
 ]
